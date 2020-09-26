bdb8f752e102a24ae60ad97ecf2e280667cf97aa
SinCatGit <sincat@126.com>
2020-09-26 21:25:34 +0800

    cli: Add argument parser Resolved #1

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,2 +1,21 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init(args):
+    print('ugit init project...')
+
+
 def main():
-    print("Welcome to ugit!")
+    args = parse_args()
+    args.func(args)
